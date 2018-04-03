f8232f82b27d3b2bb3787ffc57ff0b284116d11a
Hashim Ali <36916919+HashimAli1996@users.noreply.github.com>
2018-04-03 10:19:09 +0100

    UPDATED DB SCHEMA

---
+++ db_schema.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE  IF NOT EXISTS 'accountdb';
+USE 'accountdb';
+
+--
+-- Table structure for table 'accounts'
+--
+
+DROP TABLE IF EXISTS 'accounts';
+
+CREATE TABLE 'accounts' (
+  'acc_id' int(11) NOT NULL AUTO_INCREMENT,
+  'forename' varchar(45) DEFAULT NULL,
+  'surname' varchar(45) DEFAULT NULL,
+  'account_number' int(11) NOT NULL,
+  PRIMARY KEY ('acc_id')
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
